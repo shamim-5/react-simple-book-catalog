8a2cac56bcfe2c6a77d52a8f4d7defb70f90b44e
shamim-5 <shamim.somadder5@gmail.com>
2023-09-19 21:26:20 +0600

    navbar, content, footer for template-layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import BestBooks from "./BestBooks";
-
 const Home = () => {
   return (
     <div className="">
       <h2 className="text-3xl uppercase font-mono text-slate-700/90">Top 10 recent books</h2>
+
       <div className="my-6">
-         <BestBooks />
+        <h2>Homepage content</h2>
       </div>
     </div>
   );
