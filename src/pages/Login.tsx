b8e2f63d78f2b2a7cd8b4d158b2e58803e5863b0
shamim-5 <shamim.somadder5@gmail.com>
2023-09-21 14:52:16 +0600

    implemented authentication forms from codepen.io

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,79 @@
+import "../Login.css";
+import { useState, useEffect } from "react";
+
 const Login = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    const switchers = [...document.querySelectorAll(".switcher")];
+
+    switchers.forEach((item) => {
+      item.addEventListener("click", () => {
+        switchers.forEach((switcher) => switcher.parentElement?.classList.remove("is-active"));
+        (item.parentElement as HTMLElement).classList.add("is-active");
+      });
+    });
+
+    setIsActive(!isActive);
+    console.log(isActive);
+  }, []);
+
   return (
-    <div>
-      <h2>Login page</h2>
+    <div className="bg-[#253858]">
+      <section className="forms-section">
+        <h1 className="section-title">Authentication Forms</h1>
+        <div className="forms">
+          <div className="form-wrapper is-active">
+            <button type="button" className="switcher switcher-login">
+              Login
+              <span className="underline"></span>
+            </button>
+            <form className="form form-login">
+              <fieldset>
+                <legend>Please, enter your email and password for login.</legend>
+                <div className="input-block">
+                  <label htmlFor="login-email">E-mail</label>
+                  <input id="login-email" type="email" required />
+                </div>
+                <div className="input-block">
+                  <label htmlFor="login-password">Password</label>
+                  <input id="login-password" type="password" required />
+                </div>
+              </fieldset>
+              <button type="submit" className="btn-login">
+                Login
+              </button>
+            </form>
+          </div>
+
+          <div className="form-wrapper">
+            <button type="button" className="switcher switcher-signup">
+              Sign Up
+              <span className="underline"></span>
+            </button>
+            <form className="form form-signup">
+              <fieldset>
+                <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+                <div className="input-block">
+                  <label htmlFor="signup-email">E-mail</label>
+                  <input id="signup-email" type="email" required />
+                </div>
+                <div className="input-block">
+                  <label htmlFor="signup-password">Password</label>
+                  <input id="signup-password" type="password" required />
+                </div>
+                <div className="input-block">
+                  <label htmlFor="signup-password-confirm">Confirm password</label>
+                  <input id="signup-password-confirm" type="password" required />
+                </div>
+              </fieldset>
+              <button type="submit" className="btn-signup">
+                Continue
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
